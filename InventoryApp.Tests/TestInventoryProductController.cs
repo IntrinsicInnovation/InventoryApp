8e8670c0a71603991740ec6037d26b1f914d23b9
IntrinsicInnovation <clmhold100@hotmail.com>
2016-06-03 23:30:55 -0700

    Latest Changes-3Jun2016

---
+++ InventoryApp.Tests/TestInventoryProductController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Http.Results;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using InventoryApp.Controllers;
+using InventoryApp.Models;
+
+namespace InventoryApp.Tests
+{
+    [TestClass]
+    public class TestInventoryProductController
+    {
+        [TestMethod]
+        public void GetAllProducts_ShouldReturnAllProducts()
+        {
+            var testProducts = GetTestProducts();
+            var controller = new InventoryController(testProducts);
+
+            var result = controller.GetAllProducts() as List<Product>;
+            Assert.AreEqual(testProducts.Count, result.Count);
+        }
+
+        [TestMethod]
+        public async Task GetAllProductsAsync_ShouldReturnAllProducts()
+        {
+            var testProducts = GetTestProducts();
+            var controller = new InventoryController(testProducts);
+
+            var result = await controller.GetAllProductsAsync() as List<Product>;
+            Assert.AreEqual(testProducts.Count, result.Count);
+        }
+
+        [TestMethod]
+        public void GetProduct_ShouldReturnCorrectProduct()
+        {
+            var testProducts = GetTestProducts();
+            var controller = new InventoryController(testProducts);
+
+            var result = controller.GetProduct(4) as OkNegotiatedContentResult<Product>;
+            Assert.IsNotNull(result);
+            Assert.AreEqual(testProducts[3].Name, result.Content.Name);
+        }
+
+        [TestMethod]
+        public async Task GetProductAsync_ShouldReturnCorrectProduct()
+        {
+            var testProducts = GetTestProducts();
+            var controller = new InventoryController(testProducts);
+
+            var result = await controller.GetProductAsync(4) as OkNegotiatedContentResult<Product>;
+            Assert.IsNotNull(result);
+            Assert.AreEqual(testProducts[3].Name, result.Content.Name);
+        }
+
+        [TestMethod]
+        public void GetProduct_ShouldNotFindProduct()
+        {
+            var controller = new InventoryController(GetTestProducts());
+
+            var result = controller.GetProduct(999);
+            Assert.IsInstanceOfType(result, typeof(NotFoundResult));
+        }
+
+        [TestMethod]
+        public void PostProduct_ShouldReturnSameProduct()
+        {
+            var controller = new InventoryController(); 
+
+            var item = GetDemoProduct();
+
+            var result =
+                controller.PostProduct(item) as CreatedAtRouteNegotiatedContentResult<Product>;
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result.RouteName, "DefaultApi");
+            Assert.AreEqual(result.RouteValues["id"], result.Content.Id);
+            Assert.AreEqual(result.Content.Name, item.Name);
+        }
+
+
+        [TestMethod]
+        public void DeleteProduct_ShouldReturnOK()
+        {
+            var controller = new InventoryController();
+           
+            var item = GetDemoProduct();
+            controller.PostProduct(item);
+           
+            var result = controller.DeleteProduct(3) as OkNegotiatedContentResult<Product>;
+            Assert.IsNotNull(result);
+            Assert.AreEqual(item.Id, result.Content.Id);
+        }
+
+
+
+        Product GetDemoProduct()
+        {
+            return new Product() { Id = 3, Name = "Demo name", Price = 5 };
+        }
+
+
+
+        private List<Product> GetTestProducts()
+        {
+            var testProducts = new List<Product>();
+            testProducts.Add(new Product { Id = 1, Name = "Demo1", Price = 1 });
+            testProducts.Add(new Product { Id = 2, Name = "Demo2", Price = 3.75M });
+            testProducts.Add(new Product { Id = 3, Name = "Demo3", Price = 16.99M });
+            testProducts.Add(new Product { Id = 4, Name = "Demo4", Price = 11.00M });
+
+            return testProducts;
+        }
+    }
+}
---
+++ InventoryApp/Scripts/inventory.js
@@ -0,0 +1,22 @@
+﻿(function () { // Angular encourages module pattern, good!
+    var app = angular.module('myApp', []),
+        uri = 'api/inventory',
+        errorMessage = function (data, status) {
+            return 'Error: ' + status +
+                (data.Message !== undefined ? (' ' + data.Message) : '');
+        } 
+
+    app.controller('myCtrl', ['$http', '$scope', function ($http, $scope) {
+        
+        $scope.deleteOne = function (item) {
+            $http.delete(uri + '/' + 3) 
+                .success(function (data, status) {
+                    $scope.errorToDelete = null;
+                })
+                .error(function (data, status) {
+                    $scope.errorToDelete = errorMessage(data, status);
+                })
+        };
+      
+    }]);
+})();
